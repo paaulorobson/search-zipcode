123acdaf09a0c69cfd74c7295023eaf086203d1b
Paulo Robson Alves Fernande Neres <pauloraf@sebraesp.com.br>
2024-04-13 10:20:47 -0300

    feat: commit inicial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useZipCode } from './stores/useZipCode'
+
+const store = useZipCode()
+const { fetchCep } = store
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <h1>Busca CEP</h1>
+      <v-btn prepend-icon="$vuetify" variant="outlined" @click="fetchCep('65054005')">
+        Buscar
+      </v-btn>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped></style>
---
+++ src/models/zip-code.model.ts
@@ -0,0 +1,8 @@
+export default interface ZipCodeModel {
+  cep: string
+  logradouro: string
+  complemento: string
+  bairro: string
+  localidade: string
+  uf: string
+}
